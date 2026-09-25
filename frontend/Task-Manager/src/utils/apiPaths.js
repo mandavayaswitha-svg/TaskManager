736bca1c42595b216ea70404f154519ec56e6264
mandavayaswitha-svg <mandavayaswitha@gmail.com>
2026-09-25 20:43:24 +0530

    Update apiPaths.js

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,42 +1,40 @@
 // utils/apiPaths.js
 
-//export const BASE_URL = "http://localhost:8000";
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
 export const API_PATHS = {
   AUTH: {
-    REGISTER: "/api/auth/register", // Register a new user (Admin or Member)
-    LOGIN: "/api/auth/login", // Authenticate user & return JWT token
-    GET_PROFILE: "/api/auth/profile", // Get logged-in user details
+    REGISTER: "/api/auth/register",
+    LOGIN: "/api/auth/login",
+    GET_PROFILE: "/api/auth/profile",
   },
 
   USERS: {
-    GET_ALL_USERS: "/api/users", // Get all users (Admin only)
-    GET_USER_BY_ID: (userId) => `/api/users/${userId}`, // Get user by ID
-    CREATE_USER: "/api/users", // Create a new user (Admin only)
-    UPDATE_USER: (userId) => `/api/users/${userId}`, // Update user details
-    DELETE_USER: (userId) => `/api/users/${userId}`, // Delete a user
+    GET_ALL_USERS: "/api/users",
+    GET_USER_BY_ID: (userId) => `/api/users/${userId}`,
+    CREATE_USER: "/api/users",
+    UPDATE_USER: (userId) => `/api/users/${userId}`,
+    DELETE_USER: (userId) => `/api/users/${userId}`,
   },
 
   TASKS: {
-    GET_DASHBOARD_DATA: "/api/tasks/dashboard-data", // Get Dashboard Data
-    GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data", // Get User Dashboard Data
-    GET_ALL_TASKS: "/api/tasks", // Get all tasks (Admin: all, Users: only assigned tasks)
-    GET_TASK_BY_ID: (taskId) =>`/api/tasks/${taskId}`, // Get task by ID
-    CREATE_TASK: "/api/tasks", // Create a new task (Admin only)
-    UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`, // Update task details
-    DELETE_TASK: (taskId) => `/api/tasks/${taskId}`, // Delete a task (Admin only)
-    UPDATE_TASK_STATUS: (taskId) => `/api/tasks/${taskId}/status`, // Update task status
-    UPDATE_TODO_CHECKLIST: (taskId) => `/api/tasks/${taskId}/todo`, // Update todo checklist
+    GET_DASHBOARD_DATA: "/api/tasks/dashboard-data",
+    GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data",
+    GET_ALL_TASKS: "/api/tasks",
+    GET_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}`,
+    CREATE_TASK: "/api/tasks",
+    UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`,
+    DELETE_TASK: (taskId) => `/api/tasks/${taskId}`,
+    UPDATE_TASK_STATUS: (taskId) => `/api/tasks/${taskId}/status`,
+    UPDATE_TODO_CHECKLIST: (taskId) => `/api/tasks/${taskId}/todo`,
   },
 
   REPORTS: {
-    EXPORT_TASKS: "/api/reports/export/tasks", // Download all tasks as Excel/PDF
-    EXPORT_USERS: "/api/reports/export/users", // Download user-task report
+    EXPORT_TASKS: "/api/reports/export/tasks",
+    EXPORT_USERS: "/api/reports/export/users",
   },
+
   IMAGE: {
-        UPLOAD_IMAGE: "api/auth/upload-image",
+    UPLOAD_IMAGE: "/api/auth/upload-image",
   },
 };
+
 export default API_PATHS;
